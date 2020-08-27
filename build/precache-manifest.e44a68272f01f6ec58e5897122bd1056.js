eb87eccbbc6e7b4a055a9692cb87a7f05e0e66c4
Marcos <hz.marcosib@gmail.com>
2020-08-27 13:03:11 -0700

    added build for GH pages

---
+++ build/precache-manifest.e44a68272f01f6ec58e5897122bd1056.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252301b9dba2222cc54679c3bf0cdfff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e5de7cddd8e9b8b6691",
+    "url": "/static/css/main.93854b39.chunk.css"
+  },
+  {
+    "revision": "2bfa9dd01820df134581",
+    "url": "/static/js/2.68dbbd15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68dbbd15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5de7cddd8e9b8b6691",
+    "url": "/static/js/main.a2816d26.chunk.js"
+  },
+  {
+    "revision": "076524b2c5b339f12c41",
+    "url": "/static/js/runtime-main.f1369426.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "/static/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/static/media/add.bdfb8ebb.svg"
+  },
+  {
+    "revision": "6d113e41836a5b733297963bf1bdfe58",
+    "url": "/static/media/close.6d113e41.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/static/media/edit_button.58afbe73.svg"
+  },
+  {
+    "revision": "34b21395b4b38cd3eca51093d831c9e9",
+    "url": "/static/media/heart-icon-filled.34b21395.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/static/media/heart-icon.6c74d9b5.svg"
+  },
+  {
+    "revision": "3e6893cd2eab107d2d5385c921e95382",
+    "url": "/static/media/logo.3e6893cd.svg"
+  },
+  {
+    "revision": "efa6fb04512d4ea565aaa4109835b46d",
+    "url": "/static/media/trash.efa6fb04.svg"
+  }
+]);
